1b9af348a4674ea262a98ad5a91b8727a18ea6f7
Jack Tanner <jack@jackandtheblockstalk>
2019-12-02 22:09:01 +0100

    sha256

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include "include/person.h"
 #include "include/sovereign.h"
+#include "include/sha256.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "hello world" << endl;
-
-    Person john = Person("John");
-    cout << john.getName() << endl;
+    cout << "Create a sovereign data entity" << endl;
     
     Sovereign s = Sovereign(1111);
 
     cout << s.getData() << endl;
+    string digest = "grape";
+    cout << "sha256(" << digest << ") = " << sha256(digest) << endl;
+
     return 0;
 }
